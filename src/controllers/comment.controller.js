26b26c5fe75fa1c830fd40564a0b2d8f0371529b
vikas kumar <vikaskumar261104@gmail.com>
2026-01-28 10:52:01 +0530

    feat: Enhance comment functionality with pagination, user details, and validation checks

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,29 +1,262 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
+import { Video } from "../models/video.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+
+const isValidId = (Id, fieldName) => {
+    if (!Id?.trim() || !isValidObjectId(Id)) {
+        throw new ApiError(400, `invalid ${fieldName} Id`);
+    }
+};
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const { videoId } = req.params;
     const { page = 1, limit = 10 } = req.query;
 
-    if (!videoId?.trim() || !isValidObjectId(videoId)) {
-        throw new ApiError(400, "invalid video Id");
+    const pageInt = parseInt(page);
+    const limitInt = parseInt(limit);
+
+    isValidId(videoId, "video");
+
+    const video = await Video.findById(videoId);
+
+    if (!video || !video.isPublished) {
+        throw new ApiError(404, "video not found");
     }
+
+    const comments = await Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId),
+                parentComment: null,
+            },
+        },
+        {
+            $sort: { createdAt: -1 },
+        },
+        {
+            $skip: (pageInt - 1) * limitInt,
+        },
+        {
+            $limit: limitInt,
+        },
+        {
+            $lookup: {
+                from: "comments",
+                localField: "_id",
+                foreignField: "parentComment",
+                as: "replies",
+                pipeline: [
+                    { $sort: { createdAt: 1 } },
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1,
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        $unwind: "$owner",
+                    },
+                    {
+                        $project: {
+                            content: 1,
+                            owner: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                    {
+                        $project: {
+                            fullName: 1,
+                            username: 1,
+                            avatar: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                repliesCount: { $size: "$replies" },
+            },
+        },
+        {
+            $unwind: "$owner",
+        },
+        {
+            $project: {
+                content: 1,
+                owner: 1,
+                replies: 1,
+                repliesCount: 1,
+                createdAt: 1,
+                updatedAt: 1,
+            },
+        },
+    ]);
+
+    const totalComments = await Comment.countDocuments({
+        video: new mongoose.Types.ObjectId(videoId),
+        parentComment: null,
+    });
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {
+                comments,
+                pagination: {
+                    totalComments,
+                    currentPage: pageInt,
+                    limit: limitInt,
+                    totalPages: Math.ceil(totalComments / limitInt),
+                    hasNextPage: pageInt * limitInt < totalComments,
+                    hasPrevPage: pageInt > 1,
+                },
+            },
+            "comments fetched successfully",
+        ),
+    );
 });
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
+    const { content } = req.body;
+    const { videoId } = req.params;
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "comment cannot be empty");
+    }
+
+    isValidId(videoId, "video");
+
+    const video = await Video.findById(videoId);
+
+    if (!video || !video.isPublished) {
+        throw new ApiError(404, "video not found");
+    }
+
+    const userId = req.user._id;
+
+    const initialCommentCount = await Comment.countDocuments({
+        video: videoId,
+        owner: userId,
+    });
+
+    if (initialCommentCount >= 5) {
+        throw new ApiError(429, "more than limited comments done already");
+    }
+
+    const newComment = await Comment.create({
+        video: videoId,
+        owner: userId,
+        content: content.trim(),
+    });
+
+    if (!newComment) {
+        throw new ApiError(500, "error in registering comment");
+    }
+
+    await newComment.populate({
+        path: "owner",
+        select: "fullName avatar username",
+    });
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(201, newComment, "comment registered successfully"),
+        );
 });
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const { content } = req.body;
+    const { commentId } = req.params;
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "comment cannot be empty");
+    }
+
+    isValidId(commentId, "comment");
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "comment not found");
+    }
+
+    if (req.user._id.toString() !== comment.owner.toString()) {
+        throw new ApiError(403, "you dont have access to update this comment");
+    }
+
+    comment.content = content.trim();
+    await comment.save();
+
+    await comment.populate({
+        path: "owner",
+        select: "fullName avatar username",
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, comment, "comment edited successfully"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const { commentId } = req.params;
+    isValidId(commentId, "comment");
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "comment not found");
+    }
+
+    if (req.user._id.toString() !== comment.owner.toString()) {
+        throw new ApiError(403, "you dont have access to delete this comment");
+    }
+
+    const deleteStatus = await Comment.deleteMany({
+        $or: [{ _id: commentId }, { parentComment: commentId }],
+    });
+
+    if (deleteStatus?.acknowledged !== true) {
+        throw new ApiError(500, "error is deleting comments");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                deleteStatus.deletedCount,
+                "comments deleted successfully",
+            ),
+        );
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,96 +3,96 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-      index: true,
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String, //cloudnary url
+            required: true,
+        },
+        coverImage: {
+            type: String, //cloudnary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video",
+            },
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        refreshToken: {
+            type: String,
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    fullName: {
-      type: String,
-      required: true,
-      trim: true,
-      index: true,
-    },
-    avatar: {
-      type: String, //cloudnary url
-      required: true,
-    },
-    coverImage: {
-      type: String, //cloudnary url
-    },
-    watchHistory: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Video",
-      },
-    ],
-    password: {
-      type: String,
-      required: [true, "Password is required"],
-    },
-    refreshToken: {
-      type: String,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true },
 );
 
 //this things runs everytime just before saving
 //if any field is modified it checks if password too was modified or not only then hashes and saves it
 userSchema.pre("save", async function () {
-  //check if there is any change in password if not return
-  if (!this.isModified("password")) return;
+    //check if there is any change in password if not return
+    if (!this.isModified("password")) return;
 
-  //hash the pass
-  this.password = await bcrypt.hash(this.password, 10);
-  //pass the flag
-  //next();
+    //hash the pass
+    this.password = await bcrypt.hash(this.password, 10);
+    //pass the flag
+    //next();
 });
 
 //authenticate password
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
+    return await bcrypt.compare(password, this.password);
 };
 
 //generate access token
 userSchema.methods.generateAccessToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-      email: this.email,
-      username: this.username,
-      fullName: this.fullName,
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-    }
-  );
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        },
+    );
 };
 
 //in this we only use id as they are frequently made
 userSchema.methods.generateRefreshToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-    }
-  );
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        },
+    );
 };
 
 export const User = mongoose.model("User", userSchema);
